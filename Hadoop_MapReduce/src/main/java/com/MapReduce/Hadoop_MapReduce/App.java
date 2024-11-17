3e05016e3033651ab4af77f5c3d33e7c527e03c0
bigdata <bigdata@bigdata-virtualbox>
2024-11-17 20:21:56 +0200

    changes in reducer

--- Hadoop_MapReduce/src/main/java/com/MapReduce/Hadoop_MapReduce/App.java
+++ Hadoop_MapReduce/src/main/java/com/MapReduce/Hadoop_MapReduce/App.java
@@ -2,6 +2,8 @@ package com.MapReduce.Hadoop_MapReduce;
 
 import java.io.IOException;
 
+import javax.swing.text.html.HTMLDocument.Iterator;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
@@ -11,25 +13,28 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.util.Tool;
-import org.apache.hadoop.util.ToolRunner;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
 public class App extends Configured implements Tool {
+	
+	//prwti fasi map stelnei pair omada kai mege8os
 	public static class TeamsMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
 	Text mapKey = new Text();
 	IntWritable mapValue = new IntWritable();
 	
 		@Override
-		public void map(LongWritable key, Text value, Context context)
+		public void map(LongWritable key, Text value, Context context) 
 			throws IOException, InterruptedException {
 			String record = value.toString(); //each record
 			try {
 				String record_fields[] = record.split(";");
-				mapValue.set(1);
-				//String authors[] = record_fields[1].split("\\|"); //pipeline escape
+				String authors[] = record_fields[1].split("\\|"); //pipeline escape
 
+				
+				mapValue.set(authors.length);
 				mapKey.set(record_fields[1]);
 				context.write(mapKey, mapValue);
 			} catch (Exception e) {
@@ -37,19 +42,46 @@ public class App extends Configured implements Tool {
 			} 
 		} 
 	}
+	
+	//prwti fasi reduce stelnei pairs omades mia fora me mege8os tous kai pli8os omadwn
 	public static class TeamsReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
-		private IntWritable result = new IntWritable();
+		private IntWritable teams = new IntWritable();
+		private IntWritable teams_size = new IntWritable();
+		private int teams_counter = 0;
+		private int size = 0;
+		private Text totalTeamsKey = new Text("Total teams");
+		private Text totalSizeKey = new Text("Total size");
 		
 		@Override
 		public void reduce(Text key, Iterable<IntWritable> values, Context context)
 			throws IOException, InterruptedException {
-			int sum = 0;
-			for (IntWritable val : values) {
-				sum += val.get();
+//			int sum = 0;
+//			for (IntWritable val : values) {
+//				sum += val.get();
+//				teams++;
+//			}
+			
+			//Each number of "values" has the size of the team so we take the first.
+			IntWritable firstValue = null;
+			for(IntWritable value: values) {
+				firstValue = value;
+				size += value.get();
+				break;
 			}
-			result.set(sum);
-			context.write(key, result);
+			//int sum = values[0];
+			teams_counter++;
+			//double mo = sum/teams;
+			//result.set(sum); 
+			teams.set(teams_counter);
+			teams_size.set(size);
+			//context.write(key, result);
+			context.write(key, firstValue);
+			context.write(totalTeamsKey, teams); //elpizw auto na ftiaxnei mono ena idio pair
+			context.write(totalSizeKey, teams_size);
+			
+			
 		} 
+		
 	}
 		
 		public int run(String[] args) throws Exception {
